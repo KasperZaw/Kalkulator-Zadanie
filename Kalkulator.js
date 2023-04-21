a6131fe03ef3523d31cdb5e5488e8fb466fc60a1
KasperZaw <kacper9zawadzki@gmail.com>
2023-04-21 10:11:52 +0200

    feat: yeild and * create object [Generator]

--- Kalkulator.js
+++ Kalkulator.js
@@ -1,14 +1,18 @@
-// okay so we have c its character, we had empty function lexer  that  didn't return anything before so we were getting undefined. Now we add if statment if we got undefined
-// we getting type EOF end of file , so we showing the program hey stop reading its finished.
+// okay so we have c its character, we had empty function lexer  that  didn't return anything before so we were getting undefined. Now we add if statment, if we got undefined
+// we getting type EOF end of file , so we showing the program hey stop reading its finished there is nothing more.
+// * after function make function a genarator
+// The yield keyword is used to define a generator function, which is a special kind of function that can be used to produce a sequence of values on demand.
+const input = "7";
 
-const input = "";
+function* lexer(str){
+   
+    let c = str[0]
 
-function lexer(str){
-   let c = str[0]
    if(c === undefined) {
-      return {  type: 'EOF' }
+      yield {  type: 'EOF' }
    }
 }
+
 console.log(lexer(input))
-console.log('ok')
+
 
