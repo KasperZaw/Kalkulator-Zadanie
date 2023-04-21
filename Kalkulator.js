2d18fdc67899c149995a260c6fe91c57673f9e78
KasperZaw <kacper9zawadzki@gmail.com>
2023-04-21 11:13:02 +0200

    feat: finding number in string

--- Kalkulator.js
+++ Kalkulator.js
@@ -2,17 +2,28 @@
 // we getting type EOF end of file , so we showing the program hey stop reading its finished there is nothing more.
 // * after function make function a genarator
 // The yield keyword is used to define a generator function, which is a special kind of function that can be used to produce a sequence of values on demand..
-const input = "7";
+// !!! note for me to final project str.split('')[0].match(/sadasd/) <- regular expresion
+const input = "7777";
 
-function* lexer(str){
-   
-    let c = str[0]
-
-   if(c === undefined) {
-      yield {  type: 'EOF' }
-   }
+function* lexer(str) {
+  // looping through string and tracking current position in string
+    for (let cursor = 0; cursor <= str.length; cursor++) {
+    const c = str[cursor];
+    if (c === undefined) {
+      yield { type: "EOF" };
+    } else if (c === '7') {
+      yield {
+        type: "number",
+        value: 7,
+      }
+    }
+  }
 }
 
-console.log(lexer(input))
+console.log("start");
+// we looking for token inside input when we find it we consolelog that token in this example it will be type number value 7
+for (const token of lexer(input)) {
+  console.log(token);
+}
 
-
+console.log("finish");
